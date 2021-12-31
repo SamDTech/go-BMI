f93234bf8b3a1f484631a730946abd6a82c003f6
sam <opeyemisamuel222@gmail.com>
2021-12-31 07:26:50 +0100

    prompt the user to input their height and weight

---
+++ bmi.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Output Information
+	fmt.Println("BMI Calculator")
+	fmt.Println("---------------------------------------")
+
+	// Prompt user for input
+	fmt.Print("Please Enter your weight (kg): ")
+
+	fmt.Print("Please Enter your height (m): ")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/samdtech/bmi
+
+go 1.17
