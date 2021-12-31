f285f4d54ea7ddacccf8daec297d85f6b2079f5f
sam <opeyemisamuel222@gmail.com>
2021-12-31 08:38:59 +0100

    extract bufio to a new package

--- bmi.go
+++ bmi.go
@@ -1,28 +1,42 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"strconv"
+	"strings"
 )
 
-var reader = bufio.NewReader(os.Stdin)
+const mainTitle = "BMI Calculator"
+const separator = "---------------------------------------"
+const weightPrompt = "Please Enter your weight (kg): "
+const heightPrompt = "Please Enter your height (m): "
 
-func main(){
+func main() {
 	// Output Information
-	fmt.Println("BMI Calculator")
-	fmt.Println("---------------------------------------")
+	fmt.Println(mainTitle)
+	fmt.Println(separator)
 
 	// Prompt user for input
-	fmt.Print("Please Enter your weight (kg): ")
+	fmt.Print(weightPrompt)
 
 	weightInput, _ := reader.ReadString('\n')
 
-	fmt.Print("Please Enter your height (m): ")
-	
+	fmt.Print(heightPrompt)
 
 	heightInput, _ := reader.ReadString('\n')
 
-	fmt.Print(weightInput)
-	fmt.Print(heightInput)
+	// remove the \n in weightInput
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+
+	// remove the \n in heightInput
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
+	// calculate the bmi with the formula: weight / (height * height)
+
+	bmi := weight / (height * height)
+
+	fmt.Printf("Your BMI is %.2f\n", bmi)
 }
